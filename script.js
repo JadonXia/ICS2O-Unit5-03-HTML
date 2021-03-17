b6a34559df7f04cfe2a9610ad54a9a94976bd5cc
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-17 13:29:02 +0000

    deleted space

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //  The script below will link my button with an alert, which will be shown later.
-document.getElementById('button1').addEventListener ('click', displayalert)
+document.getElementById('button1').addEventListener('click', displayalert)
 //  The function below will cause the button to make the alert.
 function displayalert () {
   alert(document.getElementById('textbox').value)
